152e6181032cd9b767c05a71f4a3eb2b83c4fe8a
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-08-13 17:11:11 -0500

    connecting database

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ import pg from "pg";
 
 const app = express();
 
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 const { Client } = pg
 const client = new Client({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DBPORT,
-    database: process.env.DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
   })
 
 await client.connect()
@@ -19,8 +21,16 @@ await client.connect()
 
 
 
-app.get("/test", (req,res) => {
-    res.send({"test": 123});
+app.get("/test", async (req,res) => {
+
+  try {
+    const res = await client.query('SELECT * FROM testing WHERE id = $1', [1])
+    const result = res.rows;
+    console.log(result);
+  } catch (err){
+    console.log("An Error has occured", err);
+  }
+    
     //const res = await client.query('SELECT $1::text as message', ['Hello world!'])
     //console.log(res.rows[0].message) // Hello world!
     //await client.end() You use this to close the connection to the database
