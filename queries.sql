2dd679a7a8160270856886730c65dd64101cca36
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-08-27 11:51:42 -0500

    changed value of each coulmn from money to decimal(12, 2)

--- queries.sql
+++ queries.sql
@@ -2,23 +2,23 @@
 CREATE TABLE income (
     id SERIAL NOT NULL PRIMARY KEY,
     date DATE,
-    job_income money,
-    side_hustle_income money,
-    stock_income money,
-    other money,
-    total_income money
+    job_income DECIMAL(12, 2),
+    side_hustle_income DECIMAL(12, 2),
+    stock_income DECIMAL(12, 2),
+    other DECIMAL(12, 2),
+    total_income DECIMAL(12, 2)
 );
 -- Create table that has only expenses
 CREATE TABLE expense (
     id SERIAL NOT NULL PRIMARY KEY,
     date DATE,
-    housing money,
-    food money,
-    transportation money,
-    insurance money,
-    entertainment money,
-    other money,
-    total_expense money
+    housing DECIMAL(12, 2),
+    food DECIMAL(12, 2),
+    transportation DECIMAL(12, 2),
+    insurance DECIMAL(12, 2),
+    entertainment DECIMAL(12, 2),
+    other DECIMAL(12, 2),
+    total_expense DECIMAL(12, 2)
 );
 
 
