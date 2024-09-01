2aff198081050e1d0cda4f30a4e5e97c54a7605b
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-08-31 20:49:15 -0500

    reformat BarGraphDataFilter route queries to return data in the correct format json format for Recharts

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.post("/api/data/expense", async (req, res, next) => { // This will be the ex
   }
   })  
 
-  app.post("/api/data/income/period", async (req, res, next) => { 
+  app.post("/api/data/income/period", async (req, res, next) => {   // this route returns the data from income lookup
     const request = req.body;
     console.log(request);
     try {
@@ -99,14 +99,19 @@ app.post("/api/data/expense", async (req, res, next) => { // This will be the ex
       console.log("Database has looked up the 2 dates");
       console.log("Data returned from database");
       console.log(response.rows);
-      res.json(response.rows);
+      const resInc = response.rows.map((item) => {
+        return ({
+          name: "Total Income", value: Number(item.total_income) 
+        })
+      })
+      res.json(resInc);
     } catch (err){
       next(err)
       console.log("An Error has occured", err);
     }
     }) 
 
-  app.post("/api/data/expense/period", async (req, res, next) => { 
+  app.post("/api/data/expense/period", async (req, res, next) => { // this route returns the data from expense lookup
     const request = req.body;
     console.log(request);
     try {
@@ -114,24 +119,37 @@ app.post("/api/data/expense", async (req, res, next) => { // This will be the ex
       console.log("Database has looked up the 2 dates");
       console.log("Data returned from database");
       console.log(response.rows);
-      res.json(response.rows);
+      const resExp = response.rows.map((item) => {
+        return ({
+          name: "Total Expense", value: Number(item.total_expense) 
+        })
+      })
+      res.json(resExp);    
     } catch (err){
       next(err)
       console.log("An Error has occured", err);
     }
     }) 
 
-    app.post("/api/data/income-expense/period", async (req, res, next) => { 
+    app.post("/api/data/income-expense/period", async (req, res, next) => { // this route returns the data from income, and expense lookup
       const request = req.body;
       console.log(request);
       try {
         const resIncome = await client.query('SELECT * FROM income WHERE date >= $1 AND date < $2', [request.firstdate, request.seconddate]);    
         const resExpense = await client.query('SELECT * FROM expense WHERE date >= $1 AND date < $2', [request.firstdate, request.seconddate]);    
-        console.log(resIncome.rows);
-        console.log(resExpense.rows);
-        const resInc = resIncome.rows;
-        const resExp = resExpense.rows;
-        res.json({resInc, resExp});
+        console.log(resIncome.rows)
+        console.log(resExpense.rows)
+        const resInc = resIncome.rows.map((item) => {
+          return ({
+            name: "Total Income", value: Number(item.total_income) 
+          })
+        })
+        const resExp = resExpense.rows.map((item) => {
+          return ({
+            name: "Total Expense", value: Number(item.total_expense) 
+          })
+        })
+        res.json({resInc, resExp});   
       } catch (err){
         next(err)
         console.log("An Error has occured", err);
