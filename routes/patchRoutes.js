36cd96df18accd613dd376c90a853b30bb0988c7
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-16 17:25:32 -0500

    correct the sql logic for updating the exisiting row data

--- routes/patchRoutes.js
+++ routes/patchRoutes.js
@@ -8,7 +8,7 @@ router.patch("/api/data/income/:id", async (req, res, next) => {         // this
     let id = req.params.id;
     console.log(id);
     try {
-        const resIncome = await client.query('INSERT INTO income WHERE id = $1 (date, job_income, side_hustle_income, stock_income, other, total_income) VALUES ($2, $3, $4, $5, $6, $7) ', [id, request.date, request.jobincome.replace(/[\$,]/g, ''), request.sidehustleincome.replace(/[\$,]/g, ''), request.stockincome.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.totalincome.replace(/[\$,]/g, '')]);
+        const resIncome = await client.query(`UPDATE income SET date = $2, job_income = $3, side_hustle_income = $4, stock_income = $5, other = $6, total_income = $7 WHERE id = $1`, [id, request.date, request.jobincome.replace(/[\$,]/g, ''), request.sidehustleincome.replace(/[\$,]/g, ''), request.stockincome.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.totalincome.replace(/[\$,]/g, '')]);
         const resExp = resExpense.rows;
         res.json({resExp});
       } catch (err) {
@@ -22,7 +22,7 @@ router.patch("/api/data/expense/:id", async (req, res, next) => {         // thi
     let id = req.params.id
     console.log(id);
     try {
-        const resExpense = await client.query('INSERT INTO expense WHERE id = $1 (date, housing, food, transportation, insurance, entertainment, other, total_expense) VALUES ($2, $3, $4, $5, $6, $7, $8, $9)', [id, request.date, request.housing.replace(/[\$,]/g, ''), request.food.replace(/[\$,]/g, ''), request.transportation.replace(/[\$,]/g, ''), request.insurance.replace(/[\$,]/g, ''), request.entertainment.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.totalexpense.replace(/[\$,]/g, '')]);
+        const resExpense = await client.query('UPDATE expense SET date = $2, housing = $3, food = $4, transportation = $5, insurance = $6, entertainment = $7, other = $8, total_expense = $9 WHERE id = $1;', [id, request.date, request.housing.replace(/[\$,]/g, ''), request.food.replace(/[\$,]/g, ''), request.transportation.replace(/[\$,]/g, ''), request.insurance.replace(/[\$,]/g, ''), request.entertainment.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.totalexpense.replace(/[\$,]/g, '')]);
         const resExp = resExpense.rows;
         res.json({resExp});
       } catch (err) {
