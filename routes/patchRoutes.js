5c73ac2fef9ef3d65fec619680d72e23dca21227
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-18 17:37:13 -0500

    fix name of inputs provided by user to correct value

--- routes/patchRoutes.js
+++ routes/patchRoutes.js
@@ -6,11 +6,11 @@ const router = express.Router();
 router.patch("/api/data/income/:id", async (req, res, next) => {         // this route will update the income 
     let request = req.body;
     let id = req.params.id;
-    console.log(id);
     try {
-        const resIncome = await client.query(`UPDATE income SET date = $2, job_income = $3, side_hustle_income = $4, stock_income = $5, other = $6, total_income = $7 WHERE id = $1`, [id, request.date, request.jobincome.replace(/[\$,]/g, ''), request.sidehustleincome.replace(/[\$,]/g, ''), request.stockincome.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.totalincome.replace(/[\$,]/g, '')]);
-        const resExp = resExpense.rows;
-        res.json({resExp});
+        const resIncome = await client.query(`UPDATE income SET date = $2, job_income = $3, side_hustle_income = $4, stock_income = $5, other = $6, total_income = $7 WHERE id = $1`, [id, request.date, request.job_income.replace(/[\$,]/g, ''), request.side_hustle_income.replace(/[\$,]/g, ''), request.stock_income.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.total_income.replace(/[\$,]/g, '')]);
+        const resInc = resIncome;
+        res.json({resInc});
+        console.log("Income is being updated");
       } catch (err) {
         next(err)
         console.log("An Error has occured", err);
@@ -22,9 +22,10 @@ router.patch("/api/data/expense/:id", async (req, res, next) => {         // thi
     let id = req.params.id
     console.log(id);
     try {
-        const resExpense = await client.query('UPDATE expense SET date = $2, housing = $3, food = $4, transportation = $5, insurance = $6, entertainment = $7, other = $8, total_expense = $9 WHERE id = $1;', [id, request.date, request.housing.replace(/[\$,]/g, ''), request.food.replace(/[\$,]/g, ''), request.transportation.replace(/[\$,]/g, ''), request.insurance.replace(/[\$,]/g, ''), request.entertainment.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.totalexpense.replace(/[\$,]/g, '')]);
+        const resExpense = await client.query('UPDATE expense SET date = $2, housing = $3, food = $4, transportation = $5, insurance = $6, entertainment = $7, other = $8, total_expense = $9 WHERE id = $1;', [id, request.date, request.housing.replace(/[\$,]/g, ''), request.food.replace(/[\$,]/g, ''), request.transportation.replace(/[\$,]/g, ''), request.insurance.replace(/[\$,]/g, ''), request.entertainment.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.total_expense.replace(/[\$,]/g, '')]);
         const resExp = resExpense.rows;
         res.json({resExp});
+        console.log("Expense if being updated");
       } catch (err) {
         next(err)
         console.log("An Error has occured", err);
