99e72b55026a2a51abe6aee99ff0ca93f0a50368
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-20 18:27:07 -0500

    create delete route and add 2 routes to delete income and expense from db

---
+++ routes/deleteRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+import client from "../server.js";
+
+
+const router = express.Router();
+
+router.delete("/api/data/income/delete/:id", async (req, res, next) => {         // this route will delete data from the income table
+    let id = req.params.id;
+    console.log("Delete item " + id)
+        try {
+          const deleteId = await client.query('DELETE FROM income WHERE id=$1', [id]);
+          res.send("Deleted Income")
+        } catch {
+          next(err)
+          console.log("An Error has occured", err);
+        }
+});
+
+router.delete("/api/data/expense/delete/:id", async (req, res, next) => {         // this route will delete data from the expense table
+  let id = req.params.id;
+  console.log("Delete item " + id);
+        try {
+          const deleteId = await client.query('DELETE FROM expense WHERE id=$1', [id]);
+          res.send("Deleted Expense")
+        } catch {
+          next(err)
+          console.log("An Error has occured", err);
+        }
+});
+
+export default router;
