05d9a24c2b6b53704a2ef93cbe02b970e96f7548
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-10-01 15:33:19 -0500

    Remove non-error console.log statements

--- routes/patchRoutes.js
+++ routes/patchRoutes.js
@@ -10,7 +10,6 @@ router.patch("/api/data/income/:id", async (req, res, next) => {         // this
         const resIncome = await client.query(`UPDATE income SET date = $2, job_income = $3, side_hustle_income = $4, stock_income = $5, other = $6, total_income = $7 WHERE id = $1`, [id, request.date, request.job_income.replace(/[\$,]/g, ''), request.side_hustle_income.replace(/[\$,]/g, ''), request.stock_income.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.total_income.replace(/[\$,]/g, '')]);
         const resInc = resIncome;
         res.json({resInc});
-        console.log("Income is being updated");
       } catch (err) {
         next(err)
         console.log("An Error has occured", err);
@@ -24,7 +23,6 @@ router.patch("/api/data/expense/:id", async (req, res, next) => {         // thi
         const resExpense = await client.query('UPDATE expense SET date = $2, housing = $3, food = $4, transportation = $5, insurance = $6, entertainment = $7, other = $8, total_expense = $9 WHERE id = $1;', [id, request.date, request.housing.replace(/[\$,]/g, ''), request.food.replace(/[\$,]/g, ''), request.transportation.replace(/[\$,]/g, ''), request.insurance.replace(/[\$,]/g, ''), request.entertainment.replace(/[\$,]/g, ''), request.other.replace(/[\$,]/g, ''), request.total_expense.replace(/[\$,]/g, '')]);
         const resExp = resExpense.rows;
         res.json({resExp});
-        console.log("Expense if being updated");
       } catch (err) {
         next(err)
         console.log("An Error has occured", err);
